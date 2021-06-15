981f743ba08cb1cd21c51e6e6781d09306f642aa
karinariv <kaarinag_riverag@hotmail.com>
2021-06-15 11:53:21 -0500

    enlazando las vistas en app.js

--- unarbolparati/app.js
+++ unarbolparati/app.js
@@ -12,3 +12,19 @@ app.listen(3030, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('./views/index.html'));
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve('./views/login.html'));
+});
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.resolve('./views/productCart.html'));
+});
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.resolve('./views/productDetail.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve('./views/register.html'));
+});
