09316b0e3f81ee4f873db4f1202012661be28dcc
Daniel-Gualtiero <d_gualtiero@hotmail.com>
2021-07-08 13:18:13 -0500

    actualizando nombre de  json e importando en el controller

--- unarbolparati/controllers/listProdController.js
+++ unarbolparati/controllers/listProdController.js
@@ -1,26 +1,12 @@
-/* Aquí tenemos los datos de los árboles*/
 
-const listadoProductos = [
-    {
-        nombre: "Jinicuil, cuajinicuil",
-        nombre_cientifico: "Inga Jinicuil",
-        region: "Sur",
-        tipo:  "Perennifolio",
-        altura:  "15 a 20 m",
-        crecimiento: "rápido",
-        riego: "frecuente(4-5 veces por semana)",
-        suelo: "bueno y enriquecido",
-        clima: "húmedo",
-        luz: "pleno sol",
-        fruto: "comestible",
-        imagen1: '../img/inga-1.jpg',
-        imagen2: '../img/inga-1.jpg',
-    },
+/* Aquí importamos el JSON de listaProductos*/
+const fs = require('fs');
+let listProd = fs.readFileSync('../data.json', 'utf-8');
 
-]
-
+listProd = JSON.parse(listProd);
+console.log(listProd);
 
 
 //Exportamos el módulo creado en este controlador
-module.exports = listadoProductos;
+
 
