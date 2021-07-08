858b39f030e7e978c4292e135fc09ab8cc5570b0
Daniel-Gualtiero <d_gualtiero@hotmail.com>
2021-07-08 10:24:45 -0500

    creación de listado de productos en json y controller de listado opcional para pruebas

---
+++ unarbolparati/controllers/listProdController.js
@@ -0,0 +1,26 @@
+/* Aquí tenemos los datos de los árboles*/
+
+const listadoProductos = [
+    {
+        nombre: "Jinicuil, cuajinicuil",
+        nombre_cientifico: "Inga Jinicuil",
+        region: "Sur",
+        tipo:  "Perennifolio",
+        altura:  "15 a 20 m",
+        crecimiento: "rápido",
+        riego: "frecuente(4-5 veces por semana)",
+        suelo: "bueno y enriquecido",
+        clima: "húmedo",
+        luz: "pleno sol",
+        fruto: "comestible",
+        imagen1: '../img/inga-1.jpg',
+        imagen2: '../img/inga-1.jpg',
+    },
+
+]
+
+
+
+//Exportamos el módulo creado en este controlador
+module.exports = listadoProductos;
+
