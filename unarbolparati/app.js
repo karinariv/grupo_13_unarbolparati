489967872415192d7bdeaab4ecf27ed3c034332a
Daniel-Gualtiero <d_gualtiero@hotmail.com>
2021-07-13 18:15:32 -0500

    agregando código a la app.js para put y delete

--- unarbolparati/app.js
+++ unarbolparati/app.js
@@ -4,6 +4,9 @@ const mainRouter = require('./routes/mainRouter');
 
 const path = require('path');
 
+/* Para poder usar el PUT y DELETE*/
+const methodOverride = require('method-override');
+
 app.set("view engine", "ejs");
 
 app.use(express.static('public'));
@@ -33,3 +36,7 @@ app.use('/listProd', mainRouter);
 /* Para poder usar el POST y json*/
 app.use(express.urlencoder({extended:false}));
 app.use(express.json());
+
+/* Para poder usar el PUT y DELETE*/
+app.use(methodOverride('_method'));
+
